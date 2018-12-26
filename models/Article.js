064e1b299559f50204ed840a99a923b25dc0535b
SeanB <Spb4578@gmail.com>
2018-12-26 12:11:54 -0600

    new routes for articles

--- models/Article.js
+++ models/Article.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose');
-
-// eslint-disable-next-line prefer-destructuring
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-  // Title of each article.
   title: {
     type: String,
     required: true,
   },
-  // Summary for each article.
   summary: {
     type: String,
     required: true,
   },
-  // Link to each article.
   link: {
     type: String,
     required: true,
--- routes/routes.js
+++ routes/routes.js
@@ -1,85 +1,77 @@
-const express = require('express');
-
+// require all packages
+const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const mongoose = require("mongoose");
 
-const axios = require('axios');
+// Require models.
+const models = require("../models");
 
-const cheerio = require('cheerio');
-
-// Database configuration.
-const mongoose = require('mongoose');
-
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/media-net';
-
+// connect to the mongo database on local or heroku
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/media-net";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
 const db = mongoose.connection;
 
 // Check for a connection.
-db.once('open', () => {
-  console.log('Houston, we have a connection!');
+db.once("open", () => {
+  console.log(`connect to ${MONGODB_URI}`);
 });
 
 // Checking for errors.
-db.on('error', (err) => {
-  console.log('Database error: ', err);
+db.on("error", (err) => {
+  console.log("Database error: ", err);
 });
-
-// Require all models.
-const models = require('../models');
 
 // Redirects the user to the login page from root.
-router.get('/', (req, res) => {
-  res.redirect('/scrape');
+router.get("/", (req, res) => {
+  res.redirect("/scrape");
 });
 
-// Where the articles live...
-router.get('/articles', function (req, res) {
+// route to articles
+router.get("/articles", function (req, res) {
   models.Article.find().sort({ id: -1 })
-    // .populate('comment')
     .exec(function (err, articalData) {
       if (err) {
         console.log(err);
       } else {
         const hbsObject = { articles: articalData };
-        res.render('index', hbsObject);
+        res.render("index", hbsObject);
       }
     });
 });
 
-// Where the saved articles live...
-router.get('/saved', function (req, res) {
+// route to saved articles
+router.get("/saved", function (req, res) {
   models.Saved.find().sort({ id: -1 })
-    // .populate('comment')
     .exec(function (err, savedData) {
       if (err) {
         console.log(err);
       } else {
         const hbsObject = { saved: savedData };
-        res.render('saved', hbsObject);
+        res.render("saved", hbsObject);
       }
     });
 });
 
 // Where the scraping happens...
-router.get('/scrape', function (req, res) {
-  // Grabbing the site's content with axios.
-  axios.get('https://www.linuxtoday.com/').then(function (response) {
+router.get("/scrape", function (req, res) {
+  // Grabbing the site"s content with axios.
+  axios.get("https://sciworthy.com/category/sciencenews/computer-science/").then(function (response) {
     // Loading the cannon...
     const $ = cheerio.load(response.data);
 
-    $('.index').each(function (_i, _element) {
+    $(".entry-header").each(function (_i, _element) {
       const result = {};
 
       result.title = $(this)
-        .children('a')
-        .text().trim();
+        .find("span.entry-title-primary")
+        .text()
       result.summary = $(this)
-        .children('p').text().trim();
+        .find("span.entry-subtitle").text()
       result.link = $(this)
-        .children('a')
-        .attr('href').trim();
+        .children("a")
+        .attr("href")
 
       // Create a new article for each result.
       models.Article.create(result)
@@ -92,21 +84,19 @@ router.get('/scrape', function (req, res) {
     });
 
     // Redirect to the articles page.
-    res.redirect('./articles');
+    res.redirect("./articles");
   });
 });
 
 // A route to remove all scraped articles.
-router.get('/clear', function (req, res) {
-  db.collection('articles').deleteMany({});
-  console.log('Everything is gone...');
-  res.redirect('/articles');
+router.get("/clear", function (req, res) {
+  db.collection("articles").deleteMany({});
+  res.redirect("/articles");
 });
 
 // A route to save articles.
-router.get('/save/:id', function (req, res) {
+router.get("/save/:id", function (req, res) {
   models.Article.findById(req.params.id)
-    // .populate('comment')
     .exec(function (err, articalData) {
       if (err) {
         console.log(err);
@@ -127,45 +117,45 @@ router.get('/save/:id', function (req, res) {
           .catch(function (error) {
             console.log(error);
           });
-        res.redirect('/articles');
+        res.redirect("/articles");
       }
     });
 });
 
 // A route to remove all saved articles.
-router.get('/clear-all', function (req, res) {
-  db.collection('saveds').deleteMany({});
-  console.log('Everything is gone...');
-  res.redirect('/saved');
+router.get("/clear-all", function (req, res) {
+  db.collection("saveds").deleteMany({});
+  console.log("Everything is gone...");
+  res.redirect("/saved");
 });
 
 // A route to remove one saved article.
-router.get('/delete-one/:id', function (req, res) {
+router.get("/delete-one/:id", function (req, res) {
   models.Saved.findOneAndDelete({ _id: req.params.id })
     .then((doc) => {
       console.log(doc);
-      res.redirect('/saved');
+      res.redirect("/saved");
     });
 });
 
 // A route to add a note to an article.
-router.post('/note/:id', function (req, res) {
+router.post("/note/:id", function (req, res) {
   console.log(req.body.noteId);
   models.Saved.findOneAndUpdate({ _id: req.params.id }, { note: req.body.noteId })
     .then((doc) => {
       console.log(req.params.id);
       console.log(doc);
-      res.redirect('/saved');
+      res.redirect("/saved");
     });
 });
 
 // A route to delete a note to an article.
-router.get('/delete-note/:id', function (req, res) {
-  models.Saved.findOneAndUpdate({ _id: req.params.id }, { note: '' })
+router.get("/delete-note/:id", function (req, res) {
+  models.Saved.findOneAndUpdate({ _id: req.params.id }, { note: "" })
     .then((doc) => {
       console.log(req.params.id);
       console.log(doc);
-      res.redirect('/saved');
+      res.redirect("/saved");
     });
 });
 
